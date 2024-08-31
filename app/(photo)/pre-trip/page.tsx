7d3d6a5b84290a70c6f28d6756903c4001c0163b
Chan Ka Long <longchan05@gmail.com>
2024-08-31 23:04:15 +0800

    delete

--- app/(photo)/pre-trip/page.tsx
+++
@@ -1,8 +0,0 @@
-import AlbumComponent from "@/app/util/makeAlbum"; 
-import photo from '@/public/photo/pre-trip/pre-trip.json'
-
-export default function Page() {
-
-  return <AlbumComponent title={"旅程前訓練"} photo={photo} />;
-  
-}
