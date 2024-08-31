171fbde5236dd74f76935d20465c8a7eb8ad5a73
Chan Ka Long <longchan05@gmail.com>
2024-08-31 23:02:37 +0800

    changes

--- app/(photo)/san-francisco/page.tsx
+++ app/(photo)/san-francisco/page.tsx
@@ -1,8 +1,8 @@
-import AlbumComponent from "@/app/util/makeAlbumNew"; 
+import AlbumComponent from "@/app/util/makeAlbum"; 
 import photo from '@/public/photo/san-francisco/san-francisco.json'
 
 export default function Page() {
 
-  return <AlbumComponent title={"三藩市"} />;
+  return <AlbumComponent title={"三藩市"} photo={photo} />;
   
 }
