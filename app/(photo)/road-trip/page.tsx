77c86544f525def25fc3e44029862c7e79696326
Chan Ka Long <longchan05@gmail.com>
2024-08-31 22:45:11 +0800

    add

--- app/(photo)/road-trip/page.tsx
+++ app/(photo)/road-trip/page.tsx
@@ -1,8 +1,8 @@
-import AlbumComponent from "@/app/util/makeAlbumNew"; 
+import AlbumComponent from "@/app/util/makeAlbum"; 
 import photo from '@/public/photo/road-trip/road-trip.json'
 
 export default function Page() {
 
-  return <AlbumComponent title={"黃石公園"} />;
+  return <AlbumComponent title={"黃石公園"} photo={photo} />;
   
 }
