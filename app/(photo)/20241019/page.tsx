4e706a7b7d987ea574fc9de936708172bea87026
Chan Ka Long <longchan05@gmail.com>
2024-11-14 23:06:12 +0800

    Update page.tsx

--- app/(photo)/20241019/page.tsx
+++ app/(photo)/20241019/page.tsx
@@ -1,12 +1,15 @@
 import AlbumComponent from "@/app/util/makeAlbum"; 
 import photo from '@/public/photo/20241019/20241019.json'
+import Youtube from "@/app/util/makeYoutube";
 
 export default function Page() {
 
   return (
     <>
     <p className="text-3xl font-bold leading-6 text-gray-900 text-center pt-0 lg:pt-6 pb-8">10月19日</p>
-    <AlbumComponent photo={photo}  />
+    <div className="w-4/5 m-auto">
+    <Youtube youtubeSource={"https://www.youtube.com/embed/oshaTtkOC-0?si=DIM9pyQGziZM4-WX"} youtubeTitle={"hong kong pathfinder international camporee chinese theme song"} />
+    </div><AlbumComponent photo={photo}  />
     </>
   );
   
